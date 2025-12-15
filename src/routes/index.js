57043190b313a520b5c1e8b8fd23c9054475e7e3
pzcastillo <pzcastillo@filmetricscorp.com>
2025-12-15 16:40:07 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,8 @@ const auth = require('./auth');
 const accounts = require('./accounts');
 const departments = require('./departments');
 const authenticate = require('../middleware/auth');
-const enforceTenant = require('../middleware/tenant');
 
 router.use('/auth', auth);
-router.use(authenticate, enforceTenant);
 router.use('/accounts', accounts);
 router.use('/departments', departments);
 
